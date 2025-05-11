60d9a27db3dc5d19dc229008d2e6959a00662fd9
YunusscaN <bartikyunuscan@gmail.com>
2025-05-11 23:51:24 +0300

    Added Categories Service and Controllers

--- NewsCatcher.Services/Services/CategoriesService.cs
+++ NewsCatcher.Services/Services/CategoriesService.cs
@@ -7,6 +7,10 @@ using System.Security.AccessControl;
 
 namespace NewsCatcher.Services.Services
 {
+    /// <summary>
+    /// Kategorilerle ilgili işlemleri gerçekleştiren servis sınıfı.Önce veritabanı bağlantısını alır, ardından SQL komutlarını çalıştırır.
+    /// Insert Update Delete işlemleri için ExecuteNonQueryAsync, Browse işlemleri için ExecuteReader kullanır.
+    /// </summary>
     public class CategoriesService : ICategoriesService
     {
         private readonly DatabaseContext _dbContext;
@@ -14,7 +18,7 @@ namespace NewsCatcher.Services.Services
         {
             dbContext = _dbContext;
         }
-        public Task<CategoriesModel.CreateModel.Return> AddCategoryAsync(CategoriesModel.CreateModel.Request request)
+        public async Task<CategoriesModel.CreateModel.Return> AddCategoryAsync(CategoriesModel.CreateModel.Request request)
         {
             var sqlConnection = _dbContext.DatabaseConnection();
             var sqlCommand = new SqlCommand("sp_Categories_Create", sqlConnection)
@@ -22,31 +26,117 @@ namespace NewsCatcher.Services.Services
                 CommandType = CommandType.StoredProcedure
             };
             sqlCommand.Parameters.AddWithValue("@CategorieName", request.CategorieName);
-            sqlCommand.Parameters.AddWithValue("@CategorieDescription", request.CategorieDescription);      
- 
-            var result = sqlCommand.ExecuteNonQueryAsync();
-            sqlConnection.Close();
-            return result;
+            sqlCommand.Parameters.AddWithValue("@CategorieDescription", request.CategorieDescription);
+
+            await sqlCommand.ExecuteNonQueryAsync();
+
+            return new CategoriesModel.CreateModel.Return
+            {
+                Status = true,
+                Message = "Kategori Başarıyla Eklendi",
+                ErrorCode = null,
+                ErrorMessage = null,
+                RequestId = Guid.NewGuid().ToString(), // Her işlem için benzersiz bir ID oluştur   
+                StatusCode = 200,
+                RequestTime = DateTime.UtcNow,
+                ResponseTime = DateTime.UtcNow
+            };
+        }
+          
+        public async Task<CategoriesModel.DeleteModel.Return> DeleteGetCategoryAsync(CategoriesModel.DeleteModel.Request request)
+        {
+            var sqlConnection = _dbContext.DatabaseConnection();
+            var sqlCommand = new SqlCommand("sp_Categories_Delete", sqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            sqlCommand.Parameters.AddWithValue("@CategorieId", request.CategorieId);
+
+            await sqlCommand.ExecuteNonQueryAsync();
+            return new CategoriesModel.DeleteModel.Return
+            {
+                Status = true,
+                Message = "Kategori Başarıyla Silindi",
+                ErrorCode = null,
+                ErrorMessage = null,
+                RequestId = Guid.NewGuid().ToString(), // Her işlem için benzersiz bir ID oluştur
+                StatusCode = 200, // Başarılı işlem için 200 gerekli
+                RequestTime = DateTime.UtcNow,
+                ResponseTime = DateTime.UtcNow
+            };
         }
 
-        public Task<CategoriesModel.DeleteModel.Return> DeleteGetCategoryAsync(CategoriesModel.DeleteModel.Request request)
+        public async Task<CategoriesModel.BrowseModel.Return> GetCategoriesAsync(CategoriesModel.BrowseModel.Request request)
         {
-            throw new NotImplementedException();
+            var sqlConnection = _dbContext.DatabaseConnection();
+            var sqlCommand = new SqlCommand("sp_Categories_BrowseAll", sqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            
+            await sqlCommand.ExecuteReaderAsync();
+
+            return new CategoriesModel.BrowseModel.Return
+            {
+                Status = true,
+                Message = "Kategoriler Başarıyla Listelendi",
+                ErrorCode = null,
+                ErrorMessage = null,
+                RequestId = Guid.NewGuid().ToString(), // Her işlem için benzersiz bir ID oluştur
+                StatusCode = 200, 
+                RequestTime = DateTime.UtcNow,
+                ResponseTime = DateTime.UtcNow
+            };
+
         }
 
-        public Task<CategoriesModel.BrowseModel.Return> GetCategoriesAsync(CategoriesModel.BrowseModel.Request request)
+        public async Task<CategoriesModel.BrowseModel.Return> GetCategoryByIdAsync(CategoriesModel.BrowseModel.Request request)
         {
-            throw new NotImplementedException();
+            var sqlConnection = _dbContext.DatabaseConnection();
+            var sqlCommand = new SqlCommand("sp_Categories_BrowseById", sqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            sqlCommand.Parameters.AddWithValue("@CategorieId", request.CategorieId);
+
+            await sqlCommand.ExecuteReaderAsync();
+            return new CategoriesModel.BrowseModel.Return
+            {
+                Status = true,
+                Message = "Kategori Başarıyla Listelendi",
+                ErrorCode = null,
+                ErrorMessage = null,
+                RequestId = Guid.NewGuid().ToString(), 
+                StatusCode = 200, 
+                RequestTime = DateTime.UtcNow,
+                ResponseTime = DateTime.UtcNow
+            };
+
         }
 
-        public Task<CategoriesModel.BrowseModel.Return> GetCategoryByIdAsync(CategoriesModel.BrowseModel.Request request)
+        public async Task<CategoriesModel.UpdateModel.Return> UpdateCategoryAsync(CategoriesModel.UpdateModel.Request request)
         {
-            throw new NotImplementedException();
-        }
+            var sqlConnection = _dbContext.DatabaseConnection();    
+            var sqlCommand = new SqlCommand("sp_Categories_Update", sqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            sqlCommand.Parameters.AddWithValue("@CategorieId", request.CategorieId);
+            sqlCommand.Parameters.AddWithValue("@CategorieName", request.CategorieName);
 
-        public Task<CategoriesModel.UpdateModel.Return> UpdateCategoryAsync(CategoriesModel.UpdateModel.Request request)
-        {
-            throw new NotImplementedException();
+            await sqlCommand.ExecuteNonQueryAsync();
+            return new CategoriesModel.UpdateModel.Return
+            {
+                Status = true,
+                Message = "Kategori Başarıyla Güncellendi",
+                ErrorCode = null,
+                ErrorMessage = null,
+                RequestId = Guid.NewGuid().ToString(), // Her işlem için benzersiz bir ID oluştur
+                StatusCode = 200, // Başarılı işlem için 200 gerekli
+                RequestTime = DateTime.UtcNow,
+                ResponseTime = DateTime.UtcNow
+            };
         }
-    }
+    };
 }
--- NewsCatcherApi/Controllers/CategoriesController.cs
+++ NewsCatcherApi/Controllers/CategoriesController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsCatcher.Models.Models;
+using NewsCatcher.Services.Interfaces;
+using System.Threading.Tasks;
 
 namespace NewsCatcherApi.Controllers
 {
+    /// <summary>
+    /// Kategorilerle ilgili işlemleri gerçekleştiren API denetleyici sınıfı.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        
+        private ICategoriesService _categoriesService;
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            _categoriesService = categoriesService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories(CategoriesModel.BrowseModel.Request request)
+        {
+            var result = await _categoriesService.GetCategoriesAsync(request);
+            return Ok(result);
+        }
     }
 }
