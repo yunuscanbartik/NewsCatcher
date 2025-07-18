8f1ca39f37795a58433808de1bac1b4d2306c487
YunusscaN <bartikyunuscan@gmail.com>
2025-07-18 23:09:16 +0300

    Fix RabbitMQ config and add UserJobList model/config
    Corrected RabbitMQ HostName in appsettings.json to a valid IP address. Added a new "UserJobList" section for job definitions in appsettings.json. Introduced UserJobModel.cs in NewsCatcher.Models.Models to represent user jobs, mapping JSON's RssLink to the model's RssUrl property.

---
+++ NewsCatcher.Models/Models/UserJobModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsCatcher.Models.Models
+{
+    public class UserJobModel
+    {
+            public string JobId { get; set; }
+            public string JobName { get; set; }
+            public string Cron { get; set; }
+            public string RssUrl { get; set; }
+    }
+}
