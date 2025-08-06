a0477c3ffad73ae7ea3d0fd7d9ff0781d8c6e553
YunusscaN <bartikyunuscan@gmail.com>
2025-08-07 00:54:30 +0300

    Add Services and Interfaces for trigger services.

---
+++ NewsCatcher.Services/Interfaces/IBbcJobService.cs
@@ -0,0 +1,16 @@
+﻿using NewsCatcher.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsCatcher.Services.Interfaces
+{
+    public interface IBbcJobService
+    {
+        Task<List<NewsModel.BBCModel.Item>> GetRssItemsAsync(string feedUrl);
+        Task<List<NewsModel.CreateModel.ReturnData>> MapToReturnDataAsync(List<NewsModel.BBCModel.Item> bbcItems);
+        //Task<List<NewsModel.CreateModel.ReturnData>> SendQueue(List<NewsModel.CreateModel.ReturnData> returnDataList);
+    }
+}
---
+++ NewsCatcher.Services/Interfaces/IConsumerService.cs
@@ -0,0 +1,14 @@
+﻿using NewsCatcher.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsCatcher.Services.Interfaces
+{
+    public interface IConsumerService
+    {
+        Task<List<NewsModel.CreateModel.ReturnData>> SaveToDatabaseAsync(List<NewsModel.CreateModel.ReturnData> returnDataList);
+    }
+}
